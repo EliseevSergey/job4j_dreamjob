b8efb64ca315db5f2fc7793a0d2164af4c52739f
Sergey Eliseev <s.eliseev1@gmail.com>
2024-03-31 12:29:02 +0300

    transfert

---
+++ src/main/java/ru/job4j/dreamjob/repository/Sql2oUserRepository.java
@@ -0,0 +1,39 @@
+package ru.job4j.dreamjob.repository;
+
+import org.springframework.stereotype.Repository;
+import org.sql2o.Sql2o;
+import ru.job4j.dreamjob.model.User;
+import java.util.Optional;
+
+@Repository
+public class Sql2oUserRepository implements UserRepository {
+    private final Sql2o sql2o;
+
+    public Sql2oUserRepository(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public Optional<User> save(User user) {
+
+
+
+        return Optional.empty();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public Optional<User> findByEmailAndPassword(String email, String password) {
+        return Optional.empty();
+    }
+}
