5ee818245956ea5f9db5dc1de27a9bea737b9b66
EliseevSergey <s.eliseev1@gmail.com>
2024-03-31 21:53:39 +0300

    transfert

---
+++ src/main/java/ru/job4j/dreamjob/controller/UserController.java
@@ -0,0 +1,5 @@
+package ru.job4j.dreamjob.controller;
+
+public class UserController {
+
+}
--- src/main/java/ru/job4j/dreamjob/repository/Sql2oUserRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/Sql2oUserRepository.java
@@ -1,6 +1,8 @@
 package ru.job4j.dreamjob.repository;
 
 import org.springframework.stereotype.Repository;
+import org.sql2o.Connection;
+import org.sql2o.Query;
 import org.sql2o.Sql2o;
 import ru.job4j.dreamjob.model.User;
 import java.util.Optional;
@@ -15,25 +17,33 @@ public class Sql2oUserRepository implements UserRepository {
 
     @Override
     public Optional<User> save(User user) {
-
-
-
-        return Optional.empty();
+        try (Connection connection = sql2o.open()) {
+            var sql = """
+                    INSERT INTO users (email, name, password)
+                    VALUES (:email, :name, :password)
+                    """;
+            Query query = connection.createQuery(sql, true)
+                    .addParameter("email", user.getEmail())
+                    .addParameter("name", user.getName())
+                    .addParameter("password", user.getPassword());
+            int generatedId = query.executeUpdate().getKey(Integer.class);
+            user.setId(generatedId);
+            return Optional.ofNullable(user);
+        }
     }
-
-
-
-
-
-
-
-
-
-
-
 
     @Override
     public Optional<User> findByEmailAndPassword(String email, String password) {
-        return Optional.empty();
+        try (Connection connection = sql2o.open()) {
+            String sql = """
+                    SELECT * FROM users WHERE email =:email AND password=:password
+                    """;
+            Query query = connection.createQuery(sql)
+                    .addParameter("email", email)
+                    .addParameter("password", password);
+            User user = query.executeAndFetchFirst(User.class);
+            return Optional.ofNullable(user);
+        }
     }
 }
+
